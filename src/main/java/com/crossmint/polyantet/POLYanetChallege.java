ee3529d72ac9cb7c7560e3eb8a6672683c340bd5
Daniel PC <daniel.lanzav@gmail.com>
2025-08-02 16:00:22 -0400

    Initial commit; adding barebones for the project.

---
+++ src/main/java/com/crossmint/polyantet/POLYanetChallege.java
@@ -0,0 +1,13 @@
+package com.crossmint.polyantet;
+
+
+public class POLYanetChallege {
+
+    public static void main(String[] args){
+
+        System.out.println("Challenge accepted!!!");
+
+    }
+
+
+}
